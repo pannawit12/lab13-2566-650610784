442970b1337651a7bfa9909595dedbac45597d8a
Your Name <you@example.com>
2023-08-07 15:42:30 +0700

    fish

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -3,7 +3,7 @@
 import { MovieRow } from "@/components/MovieRow";
 import { movieDB } from "@/libs/movieDB";
 
-export default function SearchResultPage() {
+export default function SearchResultPage({ params }) {
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
   // const processedSearchInput = ...
 
@@ -18,13 +18,28 @@ export default function SearchResultPage() {
   );
   */
 
+  const searchInput = params.searchInput;
+  const processedSearchInput = searchInput.replaceAll("%20"," ")
+  const filteredMovies = movieDB.filter((movie) => movie.title.toLocaleLowerCase().includes(processedSearchInput.toLocaleLowerCase()));
+
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+        Searching &quot; {processedSearchInput} &quot;
       </p>
-      <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
+      <p className="fw-bold fs-4 text-center">Found {filteredMovies.length} result(s)</p>
       {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      {
+        filteredMovies.map((movie, i) => (
+          <MovieRow
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            detail={movie.detail}
+            rating={movie.rating}
+            number={i+1}
+          />
+      ))}
     </div>
   );
 }

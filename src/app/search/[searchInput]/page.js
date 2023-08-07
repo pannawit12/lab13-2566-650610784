809726e17922e7b8b1a54e3828aebf86995d8c12
Your Name <you@example.com>
2023-08-07 15:47:21 +0700

    das

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -25,7 +25,7 @@ export default function SearchResultPage({ params }) {
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; {processedSearchInput} &quot;
+        Searching &quot;{processedSearchInput} &quot;
       </p>
       <p className="fw-bold fs-4 text-center">Found {filteredMovies.length} result(s)</p>
       {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}

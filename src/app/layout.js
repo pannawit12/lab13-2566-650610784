f9de04c488ca8fd25be3c43f50fd98fdd318ad71
Your Name <you@example.com>
2023-08-07 15:45:05 +0700

    copyright

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Pannawit Setsiriwanit"
+            studentId="650610784"
           />
         </div>
       </body>
